943b74b4ef8f18da13473c65a18bcd34b00ee532
ruslan <sqwer1986@gmail.com>
2019-12-23 18:52:50 +0300

    version 5.3

--- validation.rb
+++ validation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # validation_module
-class Validation
+module Validation
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
